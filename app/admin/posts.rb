2bb8484e8f8872fbf267ac6fa421035962290688
smplyamzng <kharmon9452@gmail.com>
2018-12-29 14:40:39 -0500

    admin fixed

---
+++ app/admin/posts.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Post do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :title, :subtitle, :description
+
+	form do |f|
+	  f.inputs do
+	    f.input :title
+	    f.input :description
+	    f.input :subtitle
+	    # f.input :image, required: true, as: :file
+	  end
+	  f.actions
+	end
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
---
+++ app/admin/services.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Service do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :title, :description, :price
+
+	form do |f|
+	  f.inputs do
+	    f.input :title
+	    f.input :description
+	    f.input :price
+	    # f.input :image, required: true, as: :file
+	  end
+	  f.actions
+	end
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
